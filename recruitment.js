e6be9f6e9ee4230cf41dd40e1c81704496f2b8a6
nasserAF <nasser_af73@hotmail.com>
2024-04-17 03:45:40 +0300

    Add files via upload

---
+++ recruitment.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwvRm1XZ6XHXp_Bsm1XJcVXdkxHx351o18sXQG1yUwIhwHKHDcIo475YcTXVPdKT6yX-Q/exec'
+
+const form = document.forms['recruitment-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("شكرا لك ،،، تمت العملية بنجاح" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
