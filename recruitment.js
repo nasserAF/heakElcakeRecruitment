b03fddcc2515938f53e07e4279ad19d183615d80
nasserAF <nasser_af73@hotmail.com>
2024-04-17 03:56:55 +0300

    Add files via upload

--- recruitment.js
+++ recruitment.js
@@ -4,8 +4,13 @@ const form = document.forms['recruitment-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  // Disable the submit button immediately
+  document.getElementById('submit').disabled = true;
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("شكرا لك ،،، تمت العملية بنجاح" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
+
+
+
